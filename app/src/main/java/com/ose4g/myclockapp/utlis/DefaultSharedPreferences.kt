5bea20bb680e918a649e1e3ac9e8330e2c8b0830
Ose4g <osemudiamenitua@gmail.com>
2021-05-14 17:29:43 +0100

    set the default values that should be in the sharedPreferences

--- app/src/main/java/com/ose4g/myclockapp/utlis/DefaultSharedPreferences.kt
+++ app/src/main/java/com/ose4g/myclockapp/utlis/DefaultSharedPreferences.kt
@@ -3,6 +3,7 @@ package com.ose4g.myclockapp.utlis
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
+import com.ose4g.myclockapp.R
 import dagger.hilt.android.qualifiers.ActivityContext
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -12,11 +13,50 @@ class DefaultSharedPreferences
 {
     private lateinit var sharedPreferences:SharedPreferences
 
-    fun getSharedPrefs():SharedPreferences
+    private fun getSharedPrefs():SharedPreferences
     {
         if(!(this::sharedPreferences.isInitialized))
         {
             sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
+            if(sharedPreferences.all.isEmpty())
+            {
+                val editor:SharedPreferences.Editor = sharedPreferences.edit()
+
+                //clock values
+                editor.putString(
+                        context.getString(KEY_CLOCK_STYLE),
+                        context.resources.getStringArray(R.array.clock_style_values)[1]
+                )
+
+                editor.putBoolean(
+                        context.getString(KEY_DISPLAY_TIME_IN_SECONDS),
+                        true
+                )
+
+                //alarm values
+                editor.putString(
+                        context.getString(KEY_SILENCE_AFTER),
+                        context.resources.getStringArray(R.array.silence_after_values)[2]
+                )
+
+                editor.putString(
+                        context.getString(KEY_SNOOZE_LENGTH),
+                        context.resources.getStringArray(R.array.snooze_length_values)[14]
+                )
+
+                editor.putString(
+                        context.getString(KEY_VOLUME_BUTTONS),
+                        context.resources.getStringArray(R.array.volume_buttons_values)[2]
+                )
+
+                editor.putString(
+                        context.getString(KEY_START_WEEK_ON),
+                        context.resources.getStringArray(R.array.start_week_day_values)[1]
+                )
+
+                editor.apply()
+            }
+
         }
 
         return sharedPreferences
