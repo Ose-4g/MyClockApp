caa51034ce0285dbf25fd252358c6c8d10329964
Ose4g <osemudiamenitua@gmail.com>
2021-05-14 17:47:02 +0100

    working on showing and hiding seconds bsased on settings

--- app/src/main/java/com/ose4g/myclockapp/fragments/ClockFaceFragment.kt
+++ app/src/main/java/com/ose4g/myclockapp/fragments/ClockFaceFragment.kt
@@ -26,66 +26,36 @@ class ClockFaceFragment : Fragment() {
 
     lateinit var binding: FragmentClockFaceBinding
     lateinit var viewModel:MainActivityViewModel
+    lateinit var handler:Handler
+    lateinit var runnable: Runnable
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-
         binding = DataBindingUtil.inflate(layoutInflater,R.layout.fragment_clock_face, container,false)
-        val view =  binding.root //inflater.inflate(R.layout.fragment_clock_face, container, false)
-        viewModel = ViewModelProvider(requireActivity()).get(MainActivityViewModel::class.java)
 
-        binding.realAnalogClock
-                .setCalendar(Calendar.getInstance())
-                .setOpacity(1.0f)
-                .setShowSeconds(true).color = Color.WHITE
+        initializeValues()
 
 
-        val handler = Handler(Looper.getMainLooper())
+        return binding.root
 
-        val runnable = object: Runnable {
-            @SuppressLint("SimpleDateFormat")
-            override fun run() {
-                val is24hr = DateFormat.is24HourFormat(requireContext())
-                Log.i("is24hr", is24hr.toString())
-                var time = ""
-                when(is24hr)
-                {
-                    true->
-                    {
-                        if(binding.amOrPm.visibility != View.GONE)
-                        {
-                            binding.amOrPm.visibility = View.GONE
-                        }
-                        val simpleDateFormat = SimpleDateFormat("HH:mm:ss")
-                        time = simpleDateFormat.format(Date())
-                    }
+    }
 
-                    false->
-                    {
-                        if(binding.amOrPm.visibility != View.VISIBLE)
-                        {
-                            binding.amOrPm.visibility = View.VISIBLE
-                        }
-                        val simpleDateFormat = SimpleDateFormat("hh:mm:ss")
-                        time = simpleDateFormat.format(Date())
-                        val ampm = SimpleDateFormat("a").format(Date())
-                        binding.amOrPm.text = ampm
-                    }
-                }
-                binding.digitalTime.text = time
-                handler.postDelayed(this,10)
-            }
-        }
 
-        handler.postDelayed(runnable,1)
-        return view
-
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
     }
 
     override fun onResume() {
         super.onResume()
+        chooseClockStyle()
+        initializeTimeHandler()
+
+    }
+
+    private fun chooseClockStyle() {
         val clockStyle = viewModel.preferences.getClockStyle()
         if(clockStyle.equals("0"))
         {
@@ -97,7 +67,62 @@ class ClockFaceFragment : Fragment() {
             binding.digitalClock.visibility = View.VISIBLE
             binding.realAnalogClock.visibility = View.GONE
         }
+    }
 
+    fun initializeValues()
+    {
+        viewModel = ViewModelProvider(requireActivity()).get(MainActivityViewModel::class.java)
+
+        binding.realAnalogClock
+                .setCalendar(Calendar.getInstance())
+                .setOpacity(1.0f)
+                .setShowSeconds(true).color = Color.WHITE
+    }
+
+
+    private fun initializeTimeHandler() {
+        handler = Handler(Looper.getMainLooper())
+
+        runnable = object: Runnable {
+            override fun run() {
+                val is24hr = DateFormat.is24HourFormat(requireContext())
+                val isDisplaySeconds = viewModel.preferences.getDisplayTimeInSeconds()
+                Log.i("is24hr", is24hr.toString())
+                var time = ""
+                when(is24hr)
+                {
+                    true->
+                    {
+                        if(binding.amOrPm.visibility != View.GONE)
+                        {
+                            binding.amOrPm.visibility = View.GONE
+                        }
+                        val simpleDateFormat = SimpleDateFormat("HH:mm:ss",Locale.getDefault())
+                        time = simpleDateFormat.format(Date())
+                    }
+
+                    false->
+                    {
+                        if(binding.amOrPm.visibility != View.VISIBLE)
+                        {
+                            binding.amOrPm.visibility = View.VISIBLE
+                        }
+                        val simpleDateFormat = SimpleDateFormat("hh:mm:ss",Locale.getDefault())
+                        time = simpleDateFormat.format(Date())
+                        val ampm = SimpleDateFormat("a",Locale.getDefault()).format(Date())
+                        binding.amOrPm.text = ampm
+                    }
+                }
+                if(!isDisplaySeconds)
+                {
+                    time = time.substringBeforeLast(":")
+                }
+                binding.digitalTime.text = time
+                handler.postDelayed(this,10)
+            }
+        }
+
+        handler.postDelayed(runnable,1)
     }
 
 }
